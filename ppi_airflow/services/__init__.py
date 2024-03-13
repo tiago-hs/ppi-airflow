d389bfa1eb504a3040ae00d5767a778b692dec81
Tiago Henrique <tiago.henrique@picpay.com>
2024-03-13 18:07:00 -0300

    [refactor] move o módulo services para o modulo core

--- ppi_airflow/services/__init__.py
+++
@@ -1,3 +0,0 @@
-"""
-Interfaces para implementação dos serviços de conexão com as origens e destinos de dados.
-"""
--- ppi_airflow/services/provider.py
+++
@@ -1,6 +0,0 @@
-from ppi_airflow.services.factory import ServiceFactory
-
-
-class ServiceProvider(ServiceFactory):
-    def get(self, service_name, **kwargs):
-        return self.create(service_name, **kwargs)
